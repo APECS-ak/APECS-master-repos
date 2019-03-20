0e3e007242a71814dd278917960a0e58bca769e3
Nicole LaRoche <nlaroche129@gmail.com>
2019-03-20 11:10:00 -0800

    Adding some csvs, working on different graphs. updated r script

--- Sea_otter_foraging/forage_simple.R
+++ Sea_otter_foraging/forage_simple.R
@@ -1,15 +1,20 @@
-####### ANOVA for foraging data #####
+####### First look at foraging data #####
 
 library(ggplot2)
+library(lattice)
 require(stats)
+library(tidyverse)
+library(plyr)
 setwd("/Users/nila/Documents/UAF/RStudio/APECS/Sea_otter_foraging")
 
 ###  DATA  ###
 s.prop <- read.csv("s_prop.csv") # import a file with the male/female proportions
-
 age.prop <- read.csv("age_prop.csv")
 all.prop <- read.csv("all_prop.csv") # issue with this one, gave a warning
 year.prop <- read.csv("year_prop.csv")
+otter.prop<- read.csv("otter_by_prop.csv")
+otter.gram<- read.csv("otter_by_gram.csv", row.names=1)
+ott.sum <- read.csv("otter_sum.csv", row.names=1)
 
 # stack the "all" data
 all <- stack(all.prop)
@@ -17,22 +22,28 @@ names(all) <- c("prop", "species")
 
 
 # checking the data frame
+is.data.frame(all) # will say if this is a dataframe, should say TRUE
 dim(s.prop)
 names(s.prop)
 str(s.prop)
 str(age.prop)
 str(year.prop)
+head(otter.gram) 
 
-#sort the data
-all<- sort(all$prop, decreasing = T, na.last= T) # this didn't work, made the graph not show up
 
-# ALL #
+#sort the data (using dplyr)
+all<-arrange(all, desc(prop))
+age.prop<-arrange(age.prop, desc(prop))
+s.prop<-arrange(s.prop, desc(prop))
+year.prop<-arrange(year.prop, desc(prop))
+
+# ALL BAR GRAPH # -- I can't figure out how to sort largest to smallest
 ggplot(data = all) +
   geom_col( 
     mapping= aes(x = species, y = prop)
   )
   
-# AGE #
+# AGE BAR GRAPH #
 ggplot(data = age.prop) +
   geom_col( 
     mapping= aes(x = species, y = prop, fill = age),
@@ -53,5 +64,29 @@ ggplot(data = year.prop) +
     position = "dodge"
   )
 
+# Prey by otter # USING OTTER_SUM AND OTTER_BY_GRAM
 
+#make sure ott sum and ott gram are equal
+identical(row.names(ott.sum), row.names(otter.gram))
 
+# make one data frame
+otter <- cbind(otter.gram, Ageclass = ott.sum$Ageclass, Sex = ott.sum$Sex, Area = ott.sum$Area)
+
+#pca of just prey types
+otter.pca <- prcomp(otter[,1:11])
+summary(otter.pca)
+biplot(otter.pca)
+otter.pca
+
+ott.pc <- cbind(otter, otter.pca$x[,1:2])  # Save PC scores 
+head(ott.pc)
+
+# none of these graphs are interesting. Doesn't say anything about the variables
+ggplot(ott.pc, aes(PC1, PC2, color=factor(Ageclass))) +
+  geom_point(size=5)
+
+ggplot(ott.pc, aes(PC1, PC2, color=factor(Area))) +
+  geom_point(size=5)
+
+ggplot(ott.pc, aes(PC1, PC2, color=factor(Sex))) +
+  geom_point(size=5)
