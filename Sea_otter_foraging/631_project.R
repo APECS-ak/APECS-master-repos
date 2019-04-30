2d41c3d153c06d46c7446b0dd5a7ebe48c61b770
Nicole LaRoche <nlaroche129@gmail.com>
2019-04-30 12:15:33 -0800

    lots of changes!

--- Sea_otter_foraging/631_project.R
+++ Sea_otter_foraging/631_project.R
@@ -8,7 +8,6 @@ library(ggplot2)
 library(lattice)
 require(stats)
 library(tidyverse)
-library(plyr)
 library(scales)
 
 #load files
@@ -32,21 +31,24 @@ hist(otter.gram$clam)
 hist(otter.gram$crab)
 
 
-### Standardize to species maximum:
-MAX <- apply(otter.gram,2,max)    # Maximum values in each column
-gram.std <- scale(otter.gram, center=F, scale = MAX)
-head(gram.std)
+### Standardize to species maximum: - Don't think this will be useful in the end
+#MAX <- apply(otter.gram,2,max)    # Maximum values in each column
+#gram.std <- scale(otter.gram, center=F, scale = MAX)
+#head(gram.std)
 
-#stack gram.std
-stack.gram.std <- stack(gram.std)
-names(stack.gram.std) <- c("gram", "species")
+#stack gram.std - doesn't work
+#stack.gram.std <- stack(gram.std)
+#names(stack.gram.std) <- c("gram", "species")
 
-#combine files
-otter <- cbind(otter.gram, Ageclass = ott.sum$Ageclass, Sex = ott.sum$Sex, Area = ott.sum$Area)
+#Transform otter.gram with square root
+(otter.gram.tf<- otter.gram^.25)
+#stack transformed data
+stack.gram.tf <- stack(otter.gram.tf)
+names(stack.gram.tf) <- c("gram", "species")
 
-# plot hist of all the grams/min
-ggplot(data=stack.gram, aes(x=gram)) + 
-  geom_histogram(binwidth = 1) +
+# plot hist of all the grams/min transformed
+ggplot(data=stack.gram.tf, aes(x=gram)) + 
+  geom_histogram(binwidth = .2) +
   facet_wrap(vars(species))
 
 # info from ott.sum
@@ -55,3 +57,85 @@ plot(ott.sum$Sex, main = "Sex")
 plot(ott.sum$Ageclass, main = "Age")
 plot(ott.sum$Area, main = "Locations (year)")
 hist(ott.sum$success_rt,  main = "Histogram of Sucess rate", xlab= "", ylab= "")
+
+#combine files
+otter <- cbind(otter.gram.tf, Ageclass = ott.sum$Ageclass, Sex = ott.sum$Sex, Area = ott.sum$Area)
+otter
+# advice from Franz - gives # of 0 in each prey type
+apply(otter.gram,2,function(x) sum(x==0))
+
+
+# create Bray-curtis distances
+library(vegan)
+library(MASS)
+otter.bray <- vegdist(otter.gram.tf, method = "bray")
+round(otter.bray,2)
+# MDS - can use 2D because is simpler
+otter.nmds <- isoMDS(otter.bray, k=2) 
+#type I PERMANOVA
+adonis2(otter.bray ~ Sex + Area + Site, data=ott.sum, perm=9999)
+# type III PERMANOVA
+adonis2(otter.bray ~ Sex + Ageclass + Area + Site, data=ott.sum, by="margin", perm=9999)
+adonis2(otter.bray ~ Sex + Area + Site, data=ott.sum, by="margin", perm=999)
+
+ott.mds<-metaMDS(otter.gram.tf, k=2, try=20, autotransform = FALSE)
+ott.mds$stress
+stressplot(ott.mds)
+ordiplot(ott.mds, display=c("sites", "species"), type="t", cex=1)
+plot(ott.mds$points, col=as.numeric(ott.sum$Area), pch=16, cex=2, asp=1)
+plot(ott.mds$points, col=as.numeric(ott.sum$Sex), pch=16, cex=2, asp=1)
+plot(ott.mds$points, col=as.numeric(ott.sum$Site), pch=16, cex=2, asp=1)
+
+
+#Now lets remove worm, chiton, barnicle, and abalone (these make up a very small percent of the diet)
+ott.slim <- otter.gram.tf[,c(1:6,10)] 
+
+#combine files
+otter.slim <- cbind(ott.slim, Ageclass = ott.sum$Ageclass, Sex = ott.sum$Sex, Area = ott.sum$Area, Site = ott.sum$Site)
+otter.slim
+
+# create Bray-curtis distances
+otter.bray2 <- vegdist(ott.slim, method = "bray")
+round(otter.bray2,2)
+# MDS - can use 2D because is simpler
+otter2.nmds <- isoMDS(otter.bray2, k=2) 
+#type I PERMANOVA
+adonis2(otter.bray2 ~ Sex + Area + Site, data=ott.sum, perm=9999)
+# type III PERMANOVA
+adonis2(otter.bray2 ~ Sex + Ageclass + Area + Site, data=ott.sum, by="margin", perm=9999)
+adonis2(otter.bray2 ~ Sex + Area, data=ott.sum, by="margin", perm=9999)
+
+ott2.mds<-metaMDS(ott.slim, k=2, try=20, autotransform = FALSE)
+ott2.mds$stress
+stressplot(ott2.mds)
+ordiplot(ott2.mds, display=c("sites", "species"), type="t", cex=1)
+plot(ott2.mds$points, col=as.numeric(ott.sum$Area), pch=16, cex=2, asp=1)
+plot(ott2.mds$points, col=as.numeric(ott.sum$Sex), pch=16, cex=2, asp=1)
+plot(ott2.mds$points, col=as.numeric(ott.sum$Site), pch=16, cex=2, asp=1)
+
+
+#Now run with ONLY known sex!
+# want to filter the data
+otter.sex <- filter(otter.slim, Sex == "M"| Sex == "F")
+
+#now make a file of the distances only by removing them from ott.sex
+ott.sex <- otter.sex[,1:7]
+
+# create Bray-curtis distances
+otter.bray3 <- vegdist(ott.sex, method = "bray")
+round(otter.bray3,2)
+# MDS - can use 2D because is simpler
+otter3.nmds <- isoMDS(otter.bray3, k=2) 
+#type I PERMANOVA
+adonis2(otter.bray3 ~ Sex + Area + Site, data=otter.sex, perm=9999)
+# type III PERMANOVA
+adonis2(otter.bray3 ~ Sex + Ageclass + Area + Site, data=otter.sex, by="margin", perm=9999)
+adonis2(otter.bray3 ~ Sex + Area, data=otter.sex, by="margin", perm=9999)
+
+ott3.mds<-metaMDS(ott.sex, k=2, try=20, autotransform = FALSE)
+ott3.mds$stress
+stressplot(ott3.mds)
+ordiplot(ott3.mds, display=c("sites", "species"), type="t", cex=1)
+plot(ott3.mds$points, col=as.numeric(ott.sum$Area), pch=16, cex=2, asp=1)
+plot(ott3.mds$points, col=as.numeric(ott.sum$Sex), pch=16, cex=2, asp=1)
+plot(ott3.mds$points, col=as.numeric(ott.sum$Site), pch=16, cex=2, asp=1)
---
+++ Sea_otter_foraging/Bombing/bomb.R
@@ -0,0 +1,12 @@
+#### BOMBING DATA #####
+# Preliminary Analysis#
+
+setwd("/Users/nila/Documents/UAF/RStudio/APECS/Sea_otter_foraging")
+
+
+#load programs
+
+
+#load files
+bomb <- read.csv("Bombing/bomb.csv")
+summary <- read.csv("Bombing/summary.csv")
